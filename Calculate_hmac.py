7fd1fcedc5fe62a69d47a810f9be76caefc3fae0
Salman Khan <salman.khan@monash.edu>
2018-09-13 15:19:36 +1000

    final

--- Calculate_hmac.py
+++ Calculate_hmac.py
@@ -1,2 +1,17 @@
 from hashlib import sha256
 import hmac
+
+
+message = "Some message"
+token = "a152d6b41875459893c1303707cfe386"
+
+def create_hmac(token,message):
+    tokenarray =  bytearray(token,'utf-8')
+    string_to_sign = message.encode('utf-8')
+    hashed = hmac.new(tokenarray,string_to_sign, sha256)
+
+    return hashed
+
+digest = create_hmac(token,message).digest()
+
+print(digest)
